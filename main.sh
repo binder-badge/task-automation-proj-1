3e892df99b7ef9e9837fdf266bd36f9baded27ac
binder-badge <124085323+binder-badge@users.noreply.github.com>
2026-03-02 19:10:51 -0500

    scaffolding

--- main.sh
+++ main.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 echo "testing 123"
 echo "shriya is testing" 
+start_apm(){
+    echo "start APM execs"
+}
+sys_level_metrics(){
+    echo "collecting sys metrics $1"
+}
+proc_level_metrics(){
+    echo "Collecting proc level metrics $1"
+}
+cleanup(){
+    echo "end APM execs + script"
+}
+start_apm
+sys_level_metrics "wahoo"
+proc_level_metrics "awooga"
+cleanup
