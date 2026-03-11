ae80c232bc9a425b37ac5900a701f572582d0098
notHotTlyHard <nvp1546@g.rit.edu>
2026-03-11 14:03:37 -0400

    chat gpt comments added, need to rewrite. + proce_monitor function inplemented

--- main.sh
+++ main.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 LOCAL_IP=$(ip addr show | grep -v '127.0.0.1' | grep -oP 'inet\s\K[\d.]+' | head -n 1)
 
+############################################################
+# FUNCTION: start_apm
+#
+# Launches the 6 APM executables:
+#   APM1, APM2, APM3, APM4, APM5, APM6
+#
+# Each process is started in the background and its PID
+# is stored in the arr_pid array for later monitoring
+# and cleanup.
+############################################################
 start_apm(){
     arr_pid=()
     for i in {1..6}
@@ -12,19 +22,70 @@ start_apm(){
     echo "PIDs: ${arr_pid[@]}"
 }
 
+############################################################
+# FUNCTION: proc_level_metrics
+#
+# Continuously collects process metrics every 5 seconds.
+#
+# Metrics collected:
+#   %CPU
+#   %MEM
+#
+# Data source:
+#   ps command
+#
+# Output files:
+#   APM1_metrics.csv
+#   APM2_metrics.csv
+#   ...
+#   APM6_metrics.csv
+############################################################
+proc_level_metrics(){
+    while true;
+    do
+        for index in "${!arr_pid[@]}"
+        do
+            pid=${arr_pid[$index]}
+            process_num=$((index+1))
+
+            data_collected=$(ps -p "$pid" -o %cpu,%mem --no-headers)
+            
+            echo "$data_collected" >> "APM${process_num}_metrics.csv"
+        done
+        sleep 5
+    done
+}
+
 sys_level_metrics(){
     echo "collecting sys metrics $1"
 }
-proc_level_metrics(){
-    echo "Collecting proc level metrics $1"
-}
+
+############################################################
+# FUNCTION: cleanup
+#
+# Called when script is terminated (Ctrl+C).
+#
+# Responsibilities:
+# 1. Kill all APM processes that were spawned
+# 2. Kill background monitoring jobs
+############################################################
 cleanup(){
-    echo "end APM execs + script"
+    echo "Cleaning up..."
+    for pid in "${arr_pid[@]}"
+    do
+        kill "$pid"
+    done
+
+    #kills any background jobs
+    kill $(jobs -p) 2>/dev/null
+    echo terminated "${arr_pid[@]}"
+    exit 0
 }
 #trap cleanup EXIT will call the cleanup function when the script exits
-trap cleanup EXIT
+trap cleanup SIGINT
 
 start_apm
-sys_level_metrics "wahoo"
-proc_level_metrics "awooga"
+#monitoruing should run in background
+sys_level_metrics &
+proc_level_metrics &
 
