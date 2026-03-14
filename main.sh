c124fbbd0f8024fd245d901b0841d11cd861de88
binder-badge <124085323+binder-badge@users.noreply.github.com>
2026-03-14 02:09:27 -0400

    god please work 4

--- main.sh
+++ main.sh
@@ -141,7 +141,7 @@ start_apm
 sys_level_metrics &
 proc_level_metrics &
 
-wait $0
+wait 
 # keep script alive to make sure it receives the sigint to trigger cleanup script
 # while [[ true ]] ; do
 #     sleep 1
