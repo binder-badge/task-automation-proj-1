3ca320656aa7c6da8be2d1a88e4f3c28293a1bf8
notHotTlyHard <nvp1546@g.rit.edu>
2026-03-22 13:16:08 -0400

    updated comments

--- main.sh
+++ main.sh
@@ -1,15 +1,8 @@
 #! /bin/bash
+
 LOCAL_IP=$(ip addr show | grep -v '127.0.0.1' | grep -oP 'inet\s\K[\d.]+' | head -n 1)
-############################################################
-# FUNCTION: start_apm
-#
-# Launches the 6 APM executables:
-#   APM1, APM2, APM3, APM4, APM5, APM6
-#
-# Each process is started in the background and its PID
-# is stored in the arr_pid array for later monitoring
-# and cleanup.
-############################################################
+
+# Start six APM executables in background and record PIDs.
 start_apm(){
     arr_pid=()
     for i in {1..6}
@@ -24,24 +17,7 @@ start_apm(){
     echo "PIDs: ${arr_pid[@]}"
 }
 
-############################################################
-# FUNCTION: proc_level_metrics
-#
-# Continuously collects process metrics every 5 seconds.
-#
-# Metrics collected:
-#   %CPU
-#   %MEM
-#
-# Data source:
-#   ps command
-#
-# Output files:
-#   APM1_metrics.csv
-#   APM2_metrics.csv
-#   ...
-#   APM6_metrics.csv
-############################################################
+# Append per-process CPU and memory (%CPU, %MEM) to CSV every 5s.
 proc_level_metrics(){
     while true;
     do
@@ -50,10 +26,10 @@ proc_level_metrics(){
             pid=${arr_pid[$index]}
             process_num=$((index+1))
 
-            # data_collected=$(ps -p "$pid" -o %cpu,%mem --no-headers | tr -s " " | sed "s/ +/,/g")
+            # replace runs of spaces with commas and remove any leading comma to format ps output as CSV
             data_collected=$(ps -p "$pid" -o %cpu,%mem --no-headers | sed "s/ \+/,/g" | sed "s/^,//g")
             
-            if [ -n "$data_collected" ]; then
+            if [ -n "$data_collected" ]; then # proceed only if ps returned non-empty metrics
                 echo "$SECONDS,$data_collected" >> "APM${process_num}_metrics.csv"
             fi
         done
@@ -61,23 +37,7 @@ proc_level_metrics(){
     done
 }
 
-############################################################
-# FUNCTION: sys_level_metrics
-#
-# Continuously collects system metrics every 5 seconds.
-#
-# Metrics collected:
-#   Rx
-#   Tx
-#   KBps written to disk
-#   MBs left on system
-#
-# Data sources:
-#   ifstat, iostat, df
-#
-# Output files:
-#   system_metrics.csv
-############################################################
+# Collect network RX/TX, disk write speed, and free space every 5s.
 sys_level_metrics(){
     echo "Seconds,Rx,Tx,Write speed,Available disk capacity" > "system_metrics.csv"
 
@@ -130,15 +90,8 @@ sys_level_metrics(){
 }
 
 
-############################################################
-# FUNCTION: cleanup
-#
-# Called when script is terminated (Ctrl+C).
-#
-# Responsibilities:
-# 1. Kill all APM processes that were spawned
-# 2. Kill background monitoring jobs
-############################################################
+
+# Stop spawned APM processes and any background jobs, then exit.
 cleanup(){
     echo "Cleaning up..."
     for pid in "${arr_pid[@]}"
